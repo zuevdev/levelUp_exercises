947dbc810431e2caf7bb26bcd0d9793ae49fedc7
zyevDev <60047184+zuevdev@users.noreply.github.com>
2020-03-06 13:09:33 +0200

    Update script.js

--- cors-and-ecipes/script.js
+++ cors-and-ecipes/script.js
@@ -22,9 +22,9 @@ async function handleSubmit(e){
 function displayRecipes(recipes){
   const html = recipes.map(recipe => {
     return `
-      <div class="recipe p-2 border rounded">
-        <h2 class="text-4xl font-semibold">${recipe.title}</h2>
-        <p>${recipe.ingredients}</p>
+      <div class="bg-white recipe p-2 border rounded">
+        <h2 class="text-2xl font-semibold">${recipe.title}</h2>
+        <p class="text-xs">${recipe.ingredients}</p>
         ${recipe.thumbnail && `<img src="${recipe.thumbnail}" alt="${recipe.title} thumbnail"/>`}
         <a class="text-blue-700 underline" href="${recipe.href}">View recipe</a>
       </div>
